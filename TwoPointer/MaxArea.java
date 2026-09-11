ea1fe589257b7f2a065703687108c12ee2eba873
peter861013 <pe310186@gmail.com>
2026-09-11 15:30:45 +0800

    Add Two Pointers solutions

---
+++ TwoPointer/MaxArea.java
@@ -0,0 +1,28 @@
+public class MaxArea {
+
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int maxArea = 0;
+
+        while (left < right) {
+            int area = Math.min(height[left], height[right]) * (right - left);
+            maxArea = Math.max(maxArea, area);
+
+            if (height[left] < height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return maxArea;
+    }
+
+    public static void main(String[] args) {
+        MaxArea solution = new MaxArea();
+
+        System.out.println(solution.maxArea(new int[]{1, 8, 6, 2, 5, 4, 8, 3, 7})); // 49
+        System.out.println(solution.maxArea(new int[]{1, 1}));                       // 1
+    }
+}
