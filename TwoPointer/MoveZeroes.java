75acfd5955b64d2f74eb0022edb3933423c6688c
peter861013 <pe310186@gmail.com>
2026-09-11 15:30:45 +0800

    Add Two Pointers solutions

---
+++ TwoPointer/MoveZeroes.java
@@ -0,0 +1,28 @@
+public class MoveZeroes {
+
+    public void moveZeroes(int[] nums) {
+        int insertPos = 0;
+
+        for (int num : nums) {
+            if (num != 0) {
+                nums[insertPos++] = num;
+            }
+        }
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        MoveZeroes solution = new MoveZeroes();
+
+        int[] nums1 = {0, 1, 0, 3, 12};
+        solution.moveZeroes(nums1);
+        System.out.println(java.util.Arrays.toString(nums1)); // [1, 3, 12, 0, 0]
+
+        int[] nums2 = {0};
+        solution.moveZeroes(nums2);
+        System.out.println(java.util.Arrays.toString(nums2)); // [0]
+    }
+}
